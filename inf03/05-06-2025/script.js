2ba737505c7e072fc89b2964402f996dc09a6091
Adasieqk <adasieqk@gmail.com>
2025-11-04 23:54:57 +0100

    Update egzamin 05-06-2025

--- inf03/05-06-2025/script.js
+++ inf03/05-06-2025/script.js
@@ -1,22 +1,22 @@
-function addToCart() {
-    const file = document.getElementById('file').files[0];
-    const numberOfCopy = +document.getElementById('number').value;
-    const typeOfPaper = document.querySelector('input[type=radio]:checked').value;
-    let price = 0;
-    if (typeOfPaper == "błyszczący") {
-        price = numberOfCopy * 1.5;
-    }
-    if (typeOfPaper == "matowy") {
-        price = numberOfCopy * 2;
-    }
-    const newImg = document.createElement("img");
-    newImg.src = file.name;
-    const newP1 = document.createElement("p");
-    newP1.innerHTML = `Liczba kopii: ${numberOfCopy}`;
-    const newP2 = document.createElement("p");
-    newP2.innerHTML = `Cena: ${price}`;
-    const cart = document.getElementById("cart");
-    cart.appendChild(newImg);
-    cart.appendChild(newP1);
-    cart.appendChild(newP2);
-}
+const $ = (selector) => document.querySelector(selector);
+
+$("#button").addEventListener("click", () => {
+    const file = $("#file").files[0];
+    const number = $("#number").value;
+    const type = $("input[name='paper']:checked").value;
+
+    if (!file || !number) return alert("Prześlij plik i liczbę kopii!");
+
+    const price = number * ((type === "błyszczący") ? 1.5 : 2);
+
+    const img = document.createElement("img");
+    img.src = file.name;
+    const p1 = document.createElement("p");
+    p1.textContent = `Liczba kopii: ${number}`;
+    const p2 = document.createElement("p");
+    p2.textContent = `Cena: ${price}`;
+
+    $("#cart").appendChild(img);
+    $("#cart").appendChild(p1);
+    $("#cart").appendChild(p2);
+})
